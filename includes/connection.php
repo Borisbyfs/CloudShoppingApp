f75bf8dd41ff9b2b7f9bdfd97ded29c587aeb984
HanifKoh <76276251+HanifKoh@users.noreply.github.com>
2023-04-09 16:36:51 +0800

    Force reset tables

--- includes/connection.php
+++ includes/connection.php
@@ -13,6 +13,8 @@ $database = mysqli_select_db($connection, DB_DATABASE);
 VerifyCategoryTable($connection, DB_DATABASE);
 VerifyBrandTable($connection, DB_DATABASE);
 VerifyUserTable($connection, DB_DATABASE);
+VerifyProductTable($connection, DB_DATABASE);
+VerifyCartDetailsTable($connection, DB_DATABASE);
 
 ?>
 
@@ -25,7 +27,6 @@ function ClearAllTables($connection, $dbName)
         echo ("<p>Error dropping category table.</p>");
 
 
-
     $query = "DROP TABLE brand";
     if (!mysqli_query($connection, $query))
         echo ("<p>Error dropping brand table.</p>");
@@ -34,6 +35,16 @@ function ClearAllTables($connection, $dbName)
     $query = "DROP TABLE usertable";
     if (!mysqli_query($connection, $query))
         echo ("<p>Error dropping user table.</p>");
+
+
+    $query = "DROP TABLE product";
+    if (!mysqli_query($connection, $query))
+        echo ("<p>Error dropping product table.</p>");
+
+
+    $query = "DROP TABLE cartdetails";
+    if (!mysqli_query($connection, $query))
+        echo ("<p>Error dropping cartdetails table.</p>");
 
 }
 
@@ -70,17 +81,52 @@ function VerifyUserTable($connection, $dbName)
     if (!TableExists("usertable", $connection, $dbName)) {
         $query = "CREATE TABLE usertable (
             Id int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            Username VARCHAR(100),
+            Username VARCHAR(255),
             Email VARCHAR(100),
-            Password VARCHAR(100),
-            Image VARCHAR(100),
+            Password VARCHAR(255),
+            Image VARCHAR(255),
             IpAddress VARCHAR(100),
-            Address VARCHAR(100),
+            Address VARCHAR(255),
             Mobile VARCHAR(100)
 )";
 
         if (!mysqli_query($connection, $query))
             echo ("<p>Error creating user table.</p>");
+    }
+}
+
+function VerifyProductTable($connection, $dbName)
+{
+    if (!TableExists("product", $connection, $dbName)) {
+        $query = "CREATE TABLE product (
+            Id int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            Name VARCHAR(100),
+            Description VARCHAR(255),
+            Keywords VARCHAR(255),
+            CategoryId int(11),
+            BrandId int(11),
+            Image VARCHAR(255),
+            Price double,
+            Date DATETIME DEFAULT CURRENT_TIMESTAMP,
+            Status VARCHAR(100)
+)";
+
+        if (!mysqli_query($connection, $query))
+            echo ("<p>Error creating product table.</p>");
+    }
+}
+
+function VerifyCartDetailsTable($connection, $dbName)
+{
+    if (!TableExists("cartdetails", $connection, $dbName)) {
+        $query = "CREATE TABLE cartdetails (
+            IpAddress VARCHAR(255),
+            ProductId int(11),
+            Quantity int(100)
+)";
+
+        if (!mysqli_query($connection, $query))
+            echo ("<p>Error creating cart details table.</p>");
     }
 }
 
